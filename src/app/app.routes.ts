a1cb73050955ec25fcc63c4a0247af28397454ba
AntonioTamez <antonio.tamez@hotmail.com>
2024-08-30 19:14:17 -0600

    commit inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Page404Component } from './page-404/page-404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'page-404', component: Page404Component},
+
+];
