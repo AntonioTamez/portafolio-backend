3f3b27a878acf59f0a27e500731b4063ff3ac6ec
AntonioTamez <antonio.tamez@hotmail.com>
2024-08-31 18:23:05 -0600

    404 finalizada

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'artic-blue': '#566FA7',
+        'artic-light-blue': '#8093B8',
+      },
+    }
   },
   plugins: [],
 }
