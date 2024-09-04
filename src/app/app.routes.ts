4aef0ce146d2e15ec16a5ab5c015ddddedf444b6
AntonioTamez <antonio.tamez@hotmail.com>
2024-09-03 20:17:00 -0600

    Se agrega pagina youtube

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { Page404Component } from './page-404/page-404.component';
+import { YoutubeHomeComponent } from './pages/youtube/youtube-home/youtube-home.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'page-404', component: Page404Component},
+    {path: 'youtube', component: YoutubeHomeComponent},
 
 ];
---
+++ src/app/pages/youtube/youtube-home/youtube-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-youtube-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './youtube-home.component.html',
+  styleUrl: './youtube-home.component.scss'
+})
+export class YoutubeHomeComponent {
+
+}
